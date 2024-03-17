2b428e5853ca0b7d264e0000d9860147a53219e9
Savio-S-Morais <101912070+Savio-S-Morais@users.noreply.github.com>
2024-03-17 18:50:08 -0300

    Estrutura de repetição for
    Estudando a estrutura de repetição com variável de controle, conhecido como estrutura de repetição "for".

--- ex013/ambiente.js
+++ ex013/ambiente.js
@@ -1,3 +1,4 @@
+console.log("Estrutura while:")
 var c = 1
 while(c <= 10) {
     console.log(`Passo ${c}`)
@@ -14,8 +15,15 @@ console.log("Tudo bem")
 */
 console.log("")
 /*------------------------*/
+console.log("Estrutura do-while:")
 var c = 1
 do{
     console.log(`Passo ${c}`)
     c++
 } while (c <= 10)
+console.log("")
+/*------------------------*/
+console.log("Estrutura for:")
+for(var c = 1; c <= 10; c++) {
+    console.log(`Passo ${c}`)
+}
