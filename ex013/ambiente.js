be11c96c0bb8548acb9c84aa71075c0acabcb651
Savio-S-Morais <101912070+Savio-S-Morais@users.noreply.github.com>
2024-03-17 18:30:56 -0300

    Estrutura de repetição
    Aprendendo sobre as estruturas "while" e "do-while"

---
+++ ex013/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+while(c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+/*
+console.log("Tudo bem")
+console.log("Tudo bem")
+console.log("Tudo bem")
+console.log("Tudo bem")
+console.log("Tudo bem")
+console.log("Tudo bem")
+*/
+console.log("")
+/*------------------------*/
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
