6c1e30f8b51f2787d75bcc97a0e877b6c43e30f8
Savio-S-Morais <101912070+Savio-S-Morais@users.noreply.github.com>
2024-03-17 23:17:53 -0300

    Resolução do professor sobre o exercicio 017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,22 +1,50 @@
 function contar(){
-    var inicio = Number(document.getElementById('inpInicio').value)
-    var fim = Number(document.getElementById('inpFim').value)
-    var passo = Number(document.getElementById('inpPasso').value)
-    var resultado = document.getElementById("contagem")
+    let inicio = document.getElementById('inpInicio')
+    let fim = document.getElementById('inpFim')
+    let passo = document.getElementById('inpPasso')
+    let res = document.getElementById("contagem")
     
-    //Verficando há input vazio ou zero
-    if(inicio, fim, passo != 0 || null){
-        resultado.innerHTML = ""
-        resultado.innerHTML = "Contando: "
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados')
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = "Contando: <br>"
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(p <= 0) {
+            window.alert("Passo inválido! Considerando PASSO = 1")
+            p = 1
+        }
         //adiciona a variavel 'num' o mesmo valor da variavel 'inicio', após isso compara o valor inicial com o valor final, então printa na tela e após isso soma o valor inicial com o salto que deve ser dado.
+        if(i < f) {
+            for(let c = i; c <= f; c += p) { //Contagem crescente
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            for(let c = i; c >= f; c -= p) { //Contagem decrescente
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+
+    /*
+    if(inicio, fim, passo != 0) {
+        res.innerHTML = ""
+        res.innerHTML = "Contando: "
+        
         var contagem = document.createElement('p') //Cria uma tag p
         for(var num = inicio; num <= fim; num = num+passo){
             var contagem = document.createElement('p') //Cria uma tag p
             contagem.innerHTML = `${num} &#x1F449`
-            resultado.appendChild(contagem)
+            res.appendChild(contagem)
         }
         contagem.innerHTML = '&#x1F3C1'
     } else {
-        resultado.innerHTML = "Impossivel contar"
+        res.innerHTML = "Impossivel contar"
     }
+    */
 }
