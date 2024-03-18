f447e0820e6d6b3cce6f64775a45ca01a18f172d
Savio-S-Morais <101912070+Savio-S-Morais@users.noreply.github.com>
2024-03-17 22:23:25 -0300

    Primeira resolução do exercício proposto, sem ter visualizado a resolução do professor

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,22 @@
-alert('Hello')
+function contar(){
+    var inicio = Number(document.getElementById('inpInicio').value)
+    var fim = Number(document.getElementById('inpFim').value)
+    var passo = Number(document.getElementById('inpPasso').value)
+    var resultado = document.getElementById("contagem")
+    
+    //Verficando há input vazio ou zero
+    if(inicio, fim, passo != 0 || null){
+        resultado.innerHTML = ""
+        resultado.innerHTML = "Contando: "
+        //adiciona a variavel 'num' o mesmo valor da variavel 'inicio', após isso compara o valor inicial com o valor final, então printa na tela e após isso soma o valor inicial com o salto que deve ser dado.
+        var contagem = document.createElement('p') //Cria uma tag p
+        for(var num = inicio; num <= fim; num = num+passo){
+            var contagem = document.createElement('p') //Cria uma tag p
+            contagem.innerHTML = `${num} &#x1F449`
+            resultado.appendChild(contagem)
+        }
+        contagem.innerHTML = '&#x1F3C1'
+    } else {
+        resultado.innerHTML = "Impossivel contar"
+    }
+}
