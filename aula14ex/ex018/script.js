b340d953b49877c33297b11b500909062fe5d54f
Savio-S-Morais <101912070+Savio-S-Morais@users.noreply.github.com>
2024-03-18 21:02:19 -0300

    Resolução exercicio 018
    Primeira resolução do exercício 018 do curso de JavaScript do Curso em Vídeo

--- aula14ex/ex018/script.js
+++ aula14ex/ex018/script.js
@@ -1,1 +1,11 @@
-alert('Hello')
+function calcular() {
+    let num = Number(document.getElementById("numero").value)
+    let result = document.getElementById("result")
+
+    for(let i = 1; i <= 10; i++){
+        multi = num * i
+
+        result.innerHTML += `<p>${num} x ${i} = ${multi}</p>`
+        console.log(`${num} x ${i} = ${multi}`)
+    }
+}
