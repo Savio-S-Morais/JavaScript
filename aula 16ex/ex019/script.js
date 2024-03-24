dc6fa462aa7c8d755e1f97760327ea215f8bfd1f
Savio-S-Morais <101912070+Savio-S-Morais@users.noreply.github.com>
2024-03-24 19:52:03 -0300

    Adição da função adicionar()
    Ampliando a funcionalidade da função adicionar(), onde adiciona os valores do input dentro do array e exibe os valores dentro do select.

--- aula 16ex/ex019/script.js
+++ aula 16ex/ex019/script.js
@@ -22,8 +22,20 @@ function isLista(n, l) {
 function adicionar() {
     //Verifica se o valor dentro de número é um número, e também não deve estar na lista.
     if(isNumero(num.value) && !isLista(num.value, valores)) {
-        window.alert("Tudo Ok")
+        valores.push(Number(num.value)) //Adicionando o número do input dentro do array
+        console.log(valores)
+        let exibe = document.createElement('option') //Cria a tag option
+        exibe.text = `Valor ${num.value} adicionado` 
+        lista.appendChild(exibe) //Vincula a tag option para dentro do Select (variavel lista)
     } else {
         window.alert("Valor invalido ou já encontrado na lista.")
+    }
+}
+
+function finalizar() {
+    if(valores != 0) {
+        window.alert("Função ainda em desenvolvimento")
+    } else {
+        window.alert("Adicione valores antes de finalizar")
     }
 }
